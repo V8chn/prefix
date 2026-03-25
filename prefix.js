8ece499ff0f6a8b6e7d15d3d9056df9453767818
V8chn <sfzzz.zzzfs@gmail.com>
2026-03-25 22:36:31 +0300

    solution prefix

---
+++ prefix.js
@@ -0,0 +1,17 @@
+function longestCommonPrefix(strs) {
+    if (!strs.length || strs[0] === "")  return "";
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+
+            if (prefix === "") return prefix;
+        }
+    }
+
+    return prefix;
+};
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
